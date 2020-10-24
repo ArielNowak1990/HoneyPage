26b12a92c1d3170bbc20787978f6bdb56c4e99f8
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-24 12:52:57 +0200

    active class in menu, and all menu in MenuSmall

--- src/components/LandingPage/HomeHeads.js
+++ src/components/LandingPage/HomeHeads.js
@@ -4,29 +4,7 @@ import logo from "../../assets/Logo.png"
 import MenuSmall from "./MenuSmall";
 
 function HomeHeads() {
-    const [counters, setCounters] = useState([0,0,0])
 
-    const handleProductMain =()=> {
-    let counter = [...counters]
-    if (counter[0] === 0) {counter[0]=1} else {counter[0]=0}
-    counter[1] = 0;
-    counter[2] = 0;
-    setCounters([...counter])
-    }
-    const handleProductBee =()=> {
-        let counter = [...counters]
-        if (counter[1] === 0) {counter[1]=1} else {counter[1]=0}
-        counter[0] = 0;
-        counter[2] = 0;
-        setCounters([...counter])
-    }
-    const handleProductOther =()=> {
-        let counter = [...counters]
-        if (counter[2] === 0) {counter[2]=1} else {counter[2]=0}
-        counter[1] = 0;
-        counter[0] = 0;
-        setCounters([...counter])
-    }
 
  return (
      <header>
@@ -36,14 +14,7 @@ function HomeHeads() {
        </div>
        <Carousel/>
        <div className={"nav"}>
-           <ul className={"menu"}>
-               <ul className={"menu_productMain"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
-               <ul className={"menu_productBee"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
-               <ul className={"menu_productOther"} onClick={handleProductOther}>INNE PRODUKTY</ul>
-               <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
-               <ul className={"menu_contact"}>KONTAKT</ul>
-           </ul>
-           <MenuSmall counters={counters}/>
+           <MenuSmall/>
        </div>
      </header>
  );
--- src/components/LandingPage/MenuSmall.js
+++ src/components/LandingPage/MenuSmall.js
@@ -1,16 +1,57 @@
 import React, { useState } from 'react';
 
-function MenuSmall({counters}) {
+function MenuSmall() {
+
+    const [counters, setCounters] = useState([0,0,0])
+
+    const handleProductMain =()=> {
+        let counter = [...counters]
+        if (counter[0] === 0) {counter[0]=1} else {counter[0]=0}
+        counter[1] = 0;
+        counter[2] = 0;
+        setCounters([...counter])
+    }
+    const handleProductBee =()=> {
+        let counter = [...counters]
+        if (counter[1] === 0) {counter[1]=1} else {counter[1]=0}
+        counter[0] = 0;
+        counter[2] = 0;
+        setCounters([...counter])
+    }
+    const handleProductOther =()=> {
+        let counter = [...counters]
+        if (counter[2] === 0) {counter[2]=1} else {counter[2]=0}
+        counter[1] = 0;
+        counter[0] = 0;
+        setCounters([...counter])
+    }
 
     if (counters[0]===0 && counters[1]===0 && counters[2]===0) {
         return (
+            <>
+            <ul className={"menu"}>
+                <ul className={"menu_productMain"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
+                <ul className={"menu_productBee"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
+                <ul className={"menu_productOther"} onClick={handleProductOther}>INNE PRODUKTY</ul>
+                <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
+                <ul className={"menu_contact"}>KONTAKT</ul>
+            </ul>
             <ul className={"menu_small none"}>
             </ul>
+                </>
         );
     } else
 
     if (counters[0]===1 && counters[1]===0 && counters[2]===0) {
         return (
+            <>
+            <ul className={"menu"}>
+                <ul className={"menu_productMain active"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
+                <ul className={"menu_productBee"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
+                <ul className={"menu_productOther"} onClick={handleProductOther}>INNE PRODUKTY</ul>
+                <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
+                <ul className={"menu_contact"}>KONTAKT</ul>
+            </ul>
             <ul className={"menu_small"}>
                 <ul className={"menu_small_productMain"}>
                     <li>MIODY</li>
@@ -19,11 +60,20 @@ function MenuSmall({counters}) {
                     <li>PRZEGRYZKA WYSKOKOWA</li>
                 </ul>
             </ul>
+                </>
         );
     } else
 
     if (counters[0]===0 && counters[1]===1 && counters[2]===0) {
         return (
+            <>
+            <ul className={"menu"}>
+                <ul className={"menu_productMain"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
+                <ul className={"menu_productBee active"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
+                <ul className={"menu_productOther"} onClick={handleProductOther}>INNE PRODUKTY</ul>
+                <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
+                <ul className={"menu_contact"}>KONTAKT</ul>
+            </ul>
             <ul className={"menu_small"}>
                 <ul className={"menu_small_productBee"}>
                     <li>MATKI PSZCZELE</li>
@@ -31,21 +81,39 @@ function MenuSmall({counters}) {
                     <li>RODZINY PRZEZIMOWANE</li>
                 </ul>
             </ul>
+                </>
         );
     } else
 
     if (counters[0]===0 && counters[1]===0 && counters[2]===1) {
         return (
+            <>
+            <ul className={"menu"}>
+                <ul className={"menu_productMain"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
+                <ul className={"menu_productBee"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
+                <ul className={"menu_productOther active"} onClick={handleProductOther}>INNE PRODUKTY</ul>
+                <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
+                <ul className={"menu_contact"}>KONTAKT</ul>
+            </ul>
             <ul className={"menu_small"}>
                 <ul className={"menu_small_productOther"}>
                     <li>ULE POLIURETANOWE</li>
                     <li>KARMNIKI</li>
                 </ul>
             </ul>
+                </>
         );
     }
     else
         return (
+            <>
+                <ul className={"menu"}>
+                    <ul className={"menu_productMain"} onClick={handleProductMain}>PRODUKTY PSZCZELE</ul>
+                    <ul className={"menu_productBee"} onClick={handleProductBee}>HODOWLA PSZCZÓL</ul>
+                    <ul className={"menu_productOther"} onClick={handleProductOther}>INNE PRODUKTY</ul>
+                    <ul className={"menu_aboutUs"}>O NASZEJ PASIECE</ul>
+                    <ul className={"menu_contact"}>KONTAKT</ul>
+                </ul>
             <div>
                 <ul className={"menu_small "}>
                     <ul className={"menu_small_productOther"}>
@@ -53,6 +121,7 @@ function MenuSmall({counters}) {
                     </ul>
                 </ul>
             </div>
+                </>
         )
 
 }
