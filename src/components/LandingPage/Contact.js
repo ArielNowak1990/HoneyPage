b5b4870524ac8c42937b8b248f388675da0dc3d1
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-24 13:50:02 +0200

    New Structure with new file

---
+++ src/components/LandingPage/Contact.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import Carousel from "./Carousel";
+import logo from "../../assets/Logo.png"
+import MenuSmall from "./MenuSmall";
+
+function HomeHeads() {
+
+
+ return (
+     <header>
+       <div className={"logo_name"}>
+        <div><img src={logo} alt="logo" className={"logo"}/></div>
+        <h3><span>P</span>ASIEKA <span>K</span>OCIOLEK</h3>
+       </div>
+       <Carousel/>
+       <div className={"nav"}>
+           <MenuSmall/>
+       </div>
+     </header>
+ );
+}
+export default HomeHeads;
