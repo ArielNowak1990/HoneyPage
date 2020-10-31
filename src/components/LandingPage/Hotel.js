75e48e88c1ff30f49dda24a86e5299980f7dfcaa
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 18:03:56 +0100

    products Other done

--- src/components/LandingPage/Hotel.js
+++ src/components/LandingPage/Hotel.js
@@ -3,7 +3,8 @@ function Hotel() {
  return (
   <div className={"container_screen"}>
      <div className={"hotel"} id={"hotel"}>
-      <div> to jest ekran hotel </div>
+         <h3>Karmniki dla Owadów/ Hotele </h3>
+         <p>Hotel dla owadów, domek dla owadów, sztuczna pomoc gniazdowa – sztuczna budowla, zapewniająca różnym gatunkom bezkręgowców schronienie i możliwość budowy gniazd. Kształt, użyte materiały i rozmiary budowli zależą od jej celu i wymagań owadów, jakie chce się zwabić</p>
      </div>
   </div>
  );
--- src/components/LandingPage/Poliuretanowe.js
+++ src/components/LandingPage/Poliuretanowe.js
@@ -3,7 +3,8 @@ function Poliuretanowe() {
  return (
   <div className={"container_screen"}>
      <div className={"poliuretanowe"} id={"poliuretanowe"}>
-      <div> to jest ekran poliuretanowe </div>
+      <h3>Ule poliuretanowe</h3>
+         <p>Poliuretan jest materiałem lekkim, ale wytrzymałym. Ule z niego wykonane w pełni nadają się zatem do przewozu i pod tym względem nie odbiegają cechami od uli styropianowych. Lekkość materiału to także cecha ważna dla osób starszych czy słabszych fizycznie. W tym kontekście ule poliuretanowe mogą więc zainteresować także posiadaczy pasiek stacjonarnych o mniejszych, hobbystycznych rozmiarach</p>
      </div>
   </div>
  );
