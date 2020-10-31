bb35d1eb3828a8a04bca5a6e926f9db2779ec512
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 15:17:48 +0100

    structure products other

---
+++ src/components/LandingPage/Hotel.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+function Honey() {
+ return (
+  <div className={"container_screen"}>
+     <div className={"honey"} id={"honey"}>
+      <div> to jest ekran honey </div>
+     </div>
+  </div>
+ );
+}
+export default Honey;
