16cc61d0a47b85a36621b3ce1dd94557b786d8c4
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 16:59:35 +0100

    propolis description

--- src/components/LandingPage/Propolis.js
+++ src/components/LandingPage/Propolis.js
@@ -3,7 +3,14 @@ function Propolis() {
  return (
      <div className={"container_screen"}>
       <div className={"propolis"} id={"propolis"}>
+          <div className={"description"}>
+              <h3>Propolis</h3>
+              <p>Szereg bakterii wykazuje wrażliwość na działanie propolisu. Zaobserwowano, że propolis hamuje rozwój szczepów bakteryjnych odpornych na działanie antybiotyków.</p>
+          </div>
+          <div className={"description2"}>
+              <p>Znalazł zastosowanie w profilaktyce i leczeniu grypy, zaziębień w leczeniu zapaleń jamy ustnej i dziąseł, paradontozy, nieżytach żołądka, ochronie wątroby, zabezpiecza organizm przed zatruciem ołowiem, arsenem.</p>
 
+          </div>
       </div>
      </div>
  );
