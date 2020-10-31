76d233dd66f9c75f0744521b1ad7cb57e1811840
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 17:18:49 +0100

    all productBee

--- src/components/LandingPage/Alcohol.js
+++ src/components/LandingPage/Alcohol.js
@@ -3,7 +3,11 @@ function Alcohol() {
  return (
      <div className={"container_screen"}>
       <div className={"alcohol"} id={"alcohol"}>
-       to jest ekran alcohol
+       <div className={"description"}>
+           <h3>Miód pitny</h3>
+           <p>Nie można zapomnieć również iż produktem który otrzymujemy od pszczół jest również miód pitny.</p>
+           <p>Rozróżniamy ze względu na zawartość miodu: trójniaki, dwojaki, itp. </p>
+       </div>
       </div>
      </div>
  );
--- src/components/LandingPage/Cloud.js
+++ src/components/LandingPage/Cloud.js
@@ -3,7 +3,13 @@ function Cloud() {
  return (
      <div className={"container_screen"}>
       <div className={"cloud"} id={"cloud"}>
-       to jest ekran cloud
+          <div className={"description"}>
+              <h3>Pyłek Pszczeli</h3>
+              <p>W pyłku kwiatowym wykryto ok. 40 pierwiastków pod postacią soli mineralnych w tym znaczące ilości potasu, magnezu, żelaza i cynku. W mniejszych ilościach występuje natomiast selen, bor, srebro, pallad, platyna, cyrkon i tytan.</p>
+          </div>
+          <div className={"description2"}>
+              <p>Pyłek kwiatowy jest również źródłem witamin rozpuszczalnych zarówno w tłuszczach, jak i w wodzie, tj. A, B1, B2, B3, E, C, B6, PP, P, D, H, B12, kwas foliowy, inozytol, biotyna, kwas pantotenowy, kwercetyna.</p>
+          </div>
       </div>
      </div>
  );
