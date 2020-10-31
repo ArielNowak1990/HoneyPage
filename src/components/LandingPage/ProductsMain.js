43d3e517834e3af9db302d9367d02834718ea9f4
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 12:53:43 +0100

    logo with link to HomeHeads in products main

--- src/components/LandingPage/ProductsMain.js
+++ src/components/LandingPage/ProductsMain.js
@@ -5,18 +5,46 @@ import Honey from "./Honey";
 import Propolis from "./Propolis";
 import Cloud from "./Cloud";
 import Alcohol from "./Alcohol";
+import {Link} from "react-router-dom";
 
 function ProductsMain() {
- return (
-     <div className={"container_vertical"}>
-      <Honey/>
-      <Propolis/>
-      <Cloud/>
-      <Alcohol/>
-         <div className={"nav hidden"}>
-             <MenuSmall/>
-         </div>
-     </div>
- );
+    const [counters, setCounters] = useState([0])
+
+    const handleShowMenu =()=> {
+        let counter = [...counters]
+        if (counter[0] === 0) {counter[0]=1} else {counter[0]=0}
+        setCounters([...counter])
+    }
+    if (counters[0]===0){
+        return (
+            <div className={"container_vertical"}>
+                <Honey/>
+                <Propolis/>
+                <Cloud/>
+                <Alcohol/>
+                <div className={"nav hidden"}>
+                    <MenuSmall/>
+                </div>
+                <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
+                <div onClick={handleShowMenu}>++++++++++++</div>
+            </div>
+        );
+    }
+    if (counters[0]===1){
+        return (
+            <div className={"container_vertical"}>
+                <Honey/>
+                <Propolis/>
+                <Cloud/>
+                <Alcohol/>
+                <div className={"nav"}>
+                    <MenuSmall/>
+                </div>
+                <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
+                <div onClick={handleShowMenu}>-------------</div>
+            </div>
+        );
+    }
+
 }
 export default ProductsMain;
