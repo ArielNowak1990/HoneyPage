8d12ad9b0ff8948b0a5cb8760226748b47efcc7f
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-31 16:12:37 +0100

    all structure

--- src/components/LandingPage/AboutUs.js
+++ src/components/LandingPage/AboutUs.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import {Link} from "react-router-dom";
+import logo from "../../assets/Logo.png";
+import MenuSmall from "./MenuSmall";
 function AboutUs() {
- return (
-  <div>
-   To jest okno o naszej pasiece
-  </div>
- );
+    return (
+        <div className={"container_aboutUs"}>
+            <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
+            To jest okno aboutUS
+            <div className={"nav"}>
+                <MenuSmall/>
+            </div>
+        </div>
+    );
 }
 export default AboutUs;
--- src/components/LandingPage/Contact.js
+++ src/components/LandingPage/Contact.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import MenuSmall from "./MenuSmall";
+import {Link} from "react-router-dom";
+import logo from "../../assets/Logo.png";
 function Contact() {
  return (
-  <div>
+  <div className={"container_contact"}>
+      <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
    To jest okno Kontakt
+      <div className={"nav"}>
+          <MenuSmall/>
+      </div>
   </div>
  );
 }
