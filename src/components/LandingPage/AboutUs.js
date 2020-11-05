3ad48e93c230b2c12b8bc973e310fd185356c37a
Ariel Nowak <ariel.nowak90@gmail.com>
2020-11-05 18:46:44 +0100

    react with google maps

--- src/components/LandingPage/AboutUs.js
+++ src/components/LandingPage/AboutUs.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import logo from "../../assets/Logo.png";
 import MenuSmall from "./MenuSmall";
 function AboutUs() {
+
     return (
         <div className={"container_aboutUs"}>
             <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
