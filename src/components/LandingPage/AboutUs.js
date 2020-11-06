dddccc43c1721e32837424a1129b5350a31fa0fb
Ariel Nowak <ariel.nowak90@gmail.com>
2020-11-06 14:47:30 +0100

    maps in AboutUs

--- src/components/LandingPage/AboutUs.js
+++ src/components/LandingPage/AboutUs.js
@@ -2,25 +2,25 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import logo from "../../assets/Logo.png";
 import MenuSmall from "./MenuSmall";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import Maps from '../../scss/assets/maps.jpg'
 
 function AboutUs() {
-
-        var apiKey = {lat: 40.854885, lng: -88.081807};
 
     return (
         <div className={"container_aboutUs"}>
             <div><Link to='/'><img src={logo} alt="logo" className={"logo logo_fixed"}/></Link></div>
-            To jest okno aboutUS
             <div className={"maps"}>
-                <Map google={apiKey} zoom={14}/>
-                {/*<Map google={{*/}
-                {/*    lat: -25.363,*/}
-                {/*    lng: -88.081807*/}
-                {/*}} initialCenter={{*/}
-                {/*    lat: -25.363,*/}
-                {/*    lng: -88.081807*/}
-                {/*}} zoom={4}/>*/}
+                <img src={Maps} alt="Mapa google"/>
+                <div>Pasieka zlokalizowana jest w spokojnej i zalesionej okolicy. Nasze pszczoły korzystają z najlepszych,
+                najkwiatowszych łąk w okolicy. </div>
+            </div>
+            <div className={"maps"}>
+                <img src={Maps} alt="Mapa google"/>
+                <div>Wywożone przez nas ule korzystają z następujących miejscówek do zbierania miody: </div>
+            </div>
+            <div className={"maps"}>
+                <img src={Maps} alt="Mapa google"/>
+                <div>A oto nasza pasieka:</div>
             </div>
             <div className={"nav"}>
                 <MenuSmall/>
