bf518da51b1e65ffa0460f2bf3843f3c48f31e51
Ariel Nowak <ariel.nowak90@gmail.com>
2020-11-19 14:07:19 +0100

    display text in productsBee and productsOther

--- src/components/LandingPage/Hotel.js
+++ src/components/LandingPage/Hotel.js
@@ -3,9 +3,11 @@ function Hotel() {
  return (
   <div className={"container_screen"}>
      <div className={"hotel"} id={"hotel"}>
-         <h3>Karmniki dla Owadów/ Hotele </h3>
-         <p>Hotel dla owadów, domek dla owadów, sztuczna pomoc gniazdowa – sztuczna budowla, zapewniająca różnym gatunkom bezkręgowców schronienie i możliwość budowy gniazd. Kształt, użyte materiały i rozmiary budowli zależą od jej celu i wymagań owadów, jakie chce się zwabić</p>
-     </div>
+         <div className="text_container">
+             <h3>Karmniki dla Owadów/ Hotele </h3>
+             <p>Hotel dla owadów, domek dla owadów, sztuczna pomoc gniazdowa – sztuczna budowla, zapewniająca różnym gatunkom bezkręgowców schronienie i możliwość budowy gniazd. Kształt, użyte materiały i rozmiary budowli zależą od jej celu i wymagań owadów, jakie chce się zwabić</p>
+         </div>
+        </div>
   </div>
  );
 }
--- src/components/LandingPage/Poliuretanowe.js
+++ src/components/LandingPage/Poliuretanowe.js
@@ -1,12 +1,20 @@
 import React from 'react';
+
 function Poliuretanowe() {
- return (
-  <div className={"container_screen"}>
-     <div className={"poliuretanowe"} id={"poliuretanowe"}>
-      <h3>Ule poliuretanowe</h3>
-         <p>Poliuretan jest materiałem lekkim, ale wytrzymałym. Ule z niego wykonane w pełni nadają się zatem do przewozu i pod tym względem nie odbiegają cechami od uli styropianowych. Lekkość materiału to także cecha ważna dla osób starszych czy słabszych fizycznie. W tym kontekście ule poliuretanowe mogą więc zainteresować także posiadaczy pasiek stacjonarnych o mniejszych, hobbystycznych rozmiarach</p>
-     </div>
-  </div>
- );
+    return (
+        <div className={"container_screen"}>
+            <div className={"poliuretanowe"} id={"poliuretanowe"}>
+                <div className="text_container">
+                    <h3>Ule poliuretanowe</h3>
+                    <p>Poliuretan jest materiałem lekkim, ale wytrzymałym. Ule z niego wykonane w pełni nadają się zatem
+                        do przewozu i pod tym względem nie odbiegają cechami od uli styropianowych. Lekkość materiału to
+                        także cecha ważna dla osób starszych czy słabszych fizycznie. W tym kontekście ule poliuretanowe
+                        mogą więc zainteresować także posiadaczy pasiek stacjonarnych o mniejszych, hobbystycznych
+                        rozmiarach</p>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 export default Poliuretanowe;
