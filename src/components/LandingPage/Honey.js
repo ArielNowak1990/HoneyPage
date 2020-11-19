88175a1198c348e6e549c679ebed5682d6e22f4e
Ariel Nowak <ariel.nowak90@gmail.com>
2020-11-19 13:37:53 +0100

    change background text in products

--- src/components/LandingPage/Honey.js
+++ src/components/LandingPage/Honey.js
@@ -7,8 +7,6 @@ function Honey() {
           <h3>Honey - Miód</h3>
           <p>Dokładamy wszelkich starań, aby droga od zasklepionego plastra miodu po zakręcony słoik miodu dopuszczony do sprzedaży, była jak najlepiej przeprowadzona.</p>
           <p>Jesteśmy pod stałą kontrolą Państwowej Inspekcji Weterynaryjnej, przy czym spełniamy najostrzejsze wymogi</p>
-           </div>
-         <div className={"description2"}>
              <p>Specjalizujemy się głównie w miodzie z drzew iglastych, dlatego usytuowane miejsca są najlepszą bazą dla pozyskiwania najczystszych odmian miodów spadziowych.</p>
              <p>Nasze pasieki są rozmieszczone w kilkunastu miejscach terenów małopolskich, dlatego bogata baza pożytkowa pozwala uzyskać miody przeróżnych gatunków.</p>
              <p>Sprzedawany przez nas miód pochodzi wyłącznie z naszej pasieki, dlatego macie Państwo gwarancję, że otrzymujecie produkt najwyższej jakości.</p>
--- src/components/LandingPage/Propolis.js
+++ src/components/LandingPage/Propolis.js
@@ -6,10 +6,7 @@ function Propolis() {
           <div className={"description"}>
               <h3>Propolis</h3>
               <p>Szereg bakterii wykazuje wrażliwość na działanie propolisu. Zaobserwowano, że propolis hamuje rozwój szczepów bakteryjnych odpornych na działanie antybiotyków.</p>
-          </div>
-          <div className={"description2"}>
               <p>Znalazł zastosowanie w profilaktyce i leczeniu grypy, zaziębień w leczeniu zapaleń jamy ustnej i dziąseł, paradontozy, nieżytach żołądka, ochronie wątroby, zabezpiecza organizm przed zatruciem ołowiem, arsenem.</p>
-
           </div>
       </div>
      </div>
